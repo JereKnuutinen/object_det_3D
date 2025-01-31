3441b30456d20df08300c38cc7e00a931b05d3b8
JereKnuutinen <jere.knuutinen@hotmail.com>
2025-01-31 14:24:11 +0200

    Remove directories from tracking

--- objectdet_ws/build/merge_pointclouds/CMakeFiles/merge_pointclouds_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/merge_pointclouds_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/merge_pointclouds_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
